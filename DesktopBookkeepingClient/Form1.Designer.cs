0da08977797c3fae74e8ff7965e98eda5d84bf82
Maxim Brendel <mbrendel@twinfield.com>
2016-06-15 19:04:50 +0300

    add new doc

--- DesktopBookkeepingClient/Form1.Designer.cs
+++ DesktopBookkeepingClient/Form1.Designer.cs
@@ -28,30 +28,112 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.objectListView = new BrightIdeasSoftware.ObjectListView();
+			this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn5 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn6 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.treeListView1 = new BrightIdeasSoftware.TreeListView();
+			this.olvColumn7 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn8 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+			this.olvColumn9 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
 			((System.ComponentModel.ISupportInitialize)(this.objectListView)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.treeListView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// objectListView
 			// 
+			this.objectListView.AllColumns.Add(this.olvColumn1);
+			this.objectListView.AllColumns.Add(this.olvColumn2);
+			this.objectListView.AllColumns.Add(this.olvColumn3);
+			this.objectListView.AllColumns.Add(this.olvColumn4);
+			this.objectListView.AllColumns.Add(this.olvColumn5);
+			this.objectListView.AllColumns.Add(this.olvColumn6);
 			this.objectListView.CellEditUseWholeCell = false;
-			this.objectListView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.objectListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn1,
+            this.olvColumn2,
+            this.olvColumn3,
+            this.olvColumn4,
+            this.olvColumn5,
+            this.olvColumn6});
+			this.objectListView.Cursor = System.Windows.Forms.Cursors.Default;
+			this.objectListView.Dock = System.Windows.Forms.DockStyle.Top;
+			this.objectListView.HasCollapsibleGroups = false;
+			this.objectListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
 			this.objectListView.Location = new System.Drawing.Point(0, 0);
 			this.objectListView.Name = "objectListView";
-			this.objectListView.Size = new System.Drawing.Size(767, 262);
+			this.objectListView.Size = new System.Drawing.Size(767, 225);
 			this.objectListView.TabIndex = 0;
 			this.objectListView.UseCompatibleStateImageBehavior = false;
 			this.objectListView.View = System.Windows.Forms.View.Details;
+			// 
+			// olvColumn1
+			// 
+			this.olvColumn1.AspectName = "Date";
+			this.olvColumn1.IsVisible = false;
+			this.olvColumn1.Text = "Дата";
+			this.olvColumn1.Width = 0;
+			// 
+			// olvColumn2
+			// 
+			this.olvColumn2.AspectName = "Account";
+			this.olvColumn2.Text = "Рахунок";
+			// 
+			// olvColumn3
+			// 
+			this.olvColumn3.AspectName = "Increment";
+			this.olvColumn3.Text = "Рух";
+			// 
+			// olvColumn4
+			// 
+			this.olvColumn4.AspectName = "Supplier";
+			this.olvColumn4.Text = "Постачальник";
+			// 
+			// olvColumn5
+			// 
+			this.olvColumn5.AspectName = "Amount";
+			this.olvColumn5.Text = "Сума";
+			// 
+			// olvColumn6
+			// 
+			this.olvColumn6.AspectName = "Comment";
+			this.olvColumn6.Text = "Примітка";
+			// 
+			// treeListView1
+			// 
+			this.treeListView1.AllColumns.Add(this.olvColumn7);
+			this.treeListView1.AllColumns.Add(this.olvColumn8);
+			this.treeListView1.AllColumns.Add(this.olvColumn9);
+			this.treeListView1.CellEditUseWholeCell = false;
+			this.treeListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.olvColumn7,
+            this.olvColumn8,
+            this.olvColumn9});
+			this.treeListView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.treeListView1.Location = new System.Drawing.Point(0, 276);
+			this.treeListView1.Name = "treeListView1";
+			this.treeListView1.ShowGroups = false;
+			this.treeListView1.Size = new System.Drawing.Size(767, 267);
+			this.treeListView1.TabIndex = 1;
+			this.treeListView1.UseCompatibleStateImageBehavior = false;
+			this.treeListView1.View = System.Windows.Forms.View.Details;
+			this.treeListView1.VirtualMode = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(767, 262);
+			this.ClientSize = new System.Drawing.Size(767, 543);
+			this.Controls.Add(this.treeListView1);
 			this.Controls.Add(this.objectListView);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			((System.ComponentModel.ISupportInitialize)(this.objectListView)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.treeListView1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -59,6 +141,16 @@
 		#endregion
 
 		private BrightIdeasSoftware.ObjectListView objectListView;
+		private BrightIdeasSoftware.OLVColumn olvColumn1;
+		private BrightIdeasSoftware.OLVColumn olvColumn2;
+		private BrightIdeasSoftware.OLVColumn olvColumn3;
+		private BrightIdeasSoftware.OLVColumn olvColumn4;
+		private BrightIdeasSoftware.OLVColumn olvColumn5;
+		private BrightIdeasSoftware.OLVColumn olvColumn6;
+		private BrightIdeasSoftware.TreeListView treeListView1;
+		private BrightIdeasSoftware.OLVColumn olvColumn7;
+		private BrightIdeasSoftware.OLVColumn olvColumn8;
+		private BrightIdeasSoftware.OLVColumn olvColumn9;
 	}
 }
 
---
+++ DesktopBookkeepingClient/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopBookkeepingClient
+{
+	class Transaction
+	{
+		public DateTime Date;
+		public string Account;
+		public bool Increment;
+		public string Supplier;
+		public double Amount;
+		public string Comment;
+
+		public static List<Transaction> GetTransactions()
+		{
+			var trs = new List<Transaction>
+			{
+				new Transaction {Date = DateTime.Parse("14.06.2016"), Account = "Готівка", Increment = false, Supplier = "Сільпо", Amount = 222.22},
+				new Transaction {Date = DateTime.Parse("14.06.2016"), Account = "Готівка", Increment = false, Supplier = "Кишеня", Amount = 50.75},
+				new Transaction {Date = DateTime.Parse("15.06.2016"), Account = "Готівка", Increment = true, Supplier = "Алейка", Amount = 123.56},
+				new Transaction {Date = DateTime.Parse("15.06.2016"), Account = "Готівка", Increment = false, Supplier = "КиївПасТранс", Amount = 20.0},
+				new Transaction {Date = DateTime.Parse("15.06.2016"), Account = "Готівка", Increment = true, Supplier = "Ніколін", Amount = 150.0}
+			};
+
+			return trs;
+		}
+	}
+}
