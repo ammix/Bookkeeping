bf3d3f373978c4309b4cae22571f8602be122d5e
ammix <ammix@ukr.net>
2016-11-09 08:51:59 +0200

    Add RestSharp to ServerChecker

--- Source/BookkeepingServerChecker/Program.cs
+++ Source/BookkeepingServerChecker/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using RestSharp;
 
 namespace BookkeepingServerChecker
 {
@@ -6,8 +6,8 @@ namespace BookkeepingServerChecker
     {
         static void Main()
         {
-            var client = new RestClient("http://money.somee.com/");
-            var request = new RestRequest("api/transactions/6");
+            var client = new RestClient("http://money.somee.com");
+            var request = new RestRequest("default.asp");
 
             IRestResponse response = client.Get(request);
             var view = JsonConvert.DeserializeObject<List<FinDay>>(response.Content);
