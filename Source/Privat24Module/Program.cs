15296b4c6555ae6a89d13ad1d5a5ddd4a983bbb1
ammix <ammix@ukr.net>
2016-12-01 08:50:12 +0200

    Work on Privat24 code (put data from XML into FinTransaction)

--- Source/Privat24Module/Program.cs
+++ Source/Privat24Module/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using RestSharp;
 using System.Collections.Generic;
+using System.Web;
 
 namespace Privat24Module
 {
@@ -35,7 +36,7 @@ namespace Privat24Module
             }
         }
 
-		static string GetRequestBodyForAccountStatements(int merchantId, string password, DateTime startDate, DateTime endDate, string cardNumber)
+        static string GetRequestBodyForAccountStatements(int merchantId, string password, DateTime startDate, DateTime endDate, string cardNumber)
         {
             XDocument xml = XDocument.Parse(Resources.rest_fiz);
             var format = SaveOptions.DisableFormatting;
@@ -44,7 +45,7 @@ namespace Privat24Module
             xml.Descendants("prop").First(x => x.Attribute("name").Value == "ed").SetAttributeValue("value", endDate.ToShortDateString());
             xml.Descendants("prop").First(x => x.Attribute("name").Value == "card").SetAttributeValue("value", cardNumber);
 
-            string data = xml.Descendants("data").Select(x => x.ToString(format)).Aggregate(string.Concat);
+            string data = xml.Descendants("data").Elements().Select(x => x.ToString(format)).Aggregate(string.Concat);
             string signature = GetSignature(data, password);
 
             xml.Descendants("merchant").First().SetElementValue("id", merchantId);
@@ -55,7 +56,7 @@ namespace Privat24Module
 
         public static IEnumerable<FinTransaction> GetTransactions()
         {
-            var body = Privat24.GetRequestBodyForAccountStatements(12345, "", DateTime.Parse("1.10.2016"), DateTime.Parse("30.10.2016"), "...");
+            var body = Privat24.GetRequestBodyForAccountStatements(12345, "...", DateTime.Parse("1.10.2016"), DateTime.Parse("30.10.2016"), "...");
 
             var client = new RestClient("https://api.privatbank.ua/");
             var request = new RestRequest("p24api/rest_fiz", Method.POST);
@@ -63,7 +64,19 @@ namespace Privat24Module
 
             IRestResponse response = client.Execute(request);
             //var view = JsonConvert.DeserializeObject<List<FinDay>>(response.Content);
+
             XDocument xml = XDocument.Parse(response.Content);
+            foreach(XElement element in xml.Descendants("statements").Elements())
+            {
+                var date = element.Attribute("trandate").Value;
+                var account = element.Attribute("card").Value;
+                var amount = element.Attribute("amount").Value;
+                var note = HttpUtility.HtmlDecode(HttpUtility.HtmlDecode(element.Attribute("terminal").Value));
+                var balance = element.Attribute("rest").Value;
+                var desc = element.Attribute("description").Value;
+
+                Console.WriteLine($"{date}\t{amount}\t{balance}\t{note}\t{desc}");
+            }
 
             return null;
         }
@@ -73,9 +86,12 @@ namespace Privat24Module
     {
         static void Main(string[] args)
         {
+            Privat24.GetTransactions();
 
+            //Console.WriteLine(HttpUtility.HtmlDecode("a&amp;b"));
+            //Console.WriteLine(HttpUtility.HtmlDecode("a&#38;b"));
+            //Console.WriteLine(HttpUtility.HtmlDecode("a&quot;b"));
 
-            Console.WriteLine(response.Content);
             Console.ReadLine();
         }
     }
